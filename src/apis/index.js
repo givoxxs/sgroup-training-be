94bdad46354012dc399fee43eed41a916c34fb91
givoxxs <toanyogame@gmail.com>
2024-05-24 21:39:45 +0700

    refactoring code about apis

---
+++ src/apis/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import userRoute  from './users/user.router';
+import productRoute from './products/product.router';
+const router = express.Router();
+
+router.use('/users' ,userRoute);
+router.use('/products' ,productRoute);
+
+
+export default router;
