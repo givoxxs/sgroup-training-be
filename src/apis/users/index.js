a596413838ddbde60965213a4e6b7f3f7e648c21
Phan Văn Toàn <toanyogame@gmail.com>
2024-06-21 13:22:40 +0700

    add index in api/users folder

---
+++ src/apis/users/index.js
@@ -0,0 +1,9 @@
+import UserRoute from './user.router.js';
+import UserController from './user.controller.js';
+import UserService from './user.service.js';
+
+export {
+    UserRoute,
+    UserController,
+    UserService
+}
--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,4 +1,4 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
     host: 'localhost',
@@ -10,6 +10,23 @@ const pool = mysql.createPool({
     queueLimit: 0
 });
 
-const promisePool = pool.promise();
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.error('Database connection was closed.');
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+        console.error('Database has too many connections.');
+        }
+        if (err.code === 'ECONNREFUSED') {
+        console.error('Database connection was refused.');
+        }
+    }
+    
+    if (connection) connection.release();
+    
+    return;
+    }
+);
 
-module.exports = promisePool;
+export default pool;
